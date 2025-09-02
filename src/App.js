f15c34d671b9bccf9e950ff7db781e84dcca92c9
nandiniredddy3737 <nandinireddy3737@gmail.com>
2025-09-02 19:14:16 +0530

    Added homepage field in package.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Generate array of first 10 natural numbers (1 to 10)
+  const naturalNumbers = Array.from({ length: 10 }, (_, index) => index + 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First 10 Natural Numbers</h1>
+      <div>
+        {naturalNumbers.map((number) => (
+          <span key={number}>{number} </span>
+        ))}
+      </div>
     </div>
   );
 }
